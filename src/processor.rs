b4fb3641b8b5de4d443707bcecdeb671146b78b0
lamafab <42901763+lamafab@users.noreply.github.com>
2021-09-29 22:42:39 +0200

    remove deprecated methods

--- src/processor.rs
+++ src/processor.rs
@@ -27,12 +27,6 @@ impl AlertContext {
             should_escalate: should_escalate,
         }
     }
-    pub fn from_bytes(slice: &[u8]) -> Result<Self> {
-        serde_json::from_slice(slice).map_err(|err| err.into())
-    }
-    pub fn to_bytes(&self) -> Vec<u8> {
-        serde_json::to_vec(self).unwrap()
-    }
     pub fn should_escalate(&self) -> bool {
         self.should_escalate
     }
