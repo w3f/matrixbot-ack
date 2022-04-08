df59a4b858a85a0736a23d20957a50915872d9dc
lamafab <42901763+lamafab@users.noreply.github.com>
2022-04-08 16:57:23 +0200

    handle full list of alerts for each service

--- src/adapter/pagerduty.rs
+++ src/adapter/pagerduty.rs
@@ -1,69 +1,73 @@
-use crate::Result;
+use crate::processor::AlertContextTrimmed;
 use crate::processor::NotifyAlert;
-use crate::processor::AlertContext;
+use crate::AlertId;
+use crate::Result;
 use actix::prelude::*;
 use actix::SystemService;
 
 /// Alert Event for the PagerDuty API.
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize)]
 pub struct AlertEvent {
-	routing_key: String,
-	event_action: EventAction,
-	#[serde(rename = "payload.summary")]
-	payload_summary: String,
-	#[serde(rename = "payload.source")]
-	payload_source: String,
-	#[serde(rename = "payload.severity")]
-	payload_severity: PayloadSeverity,
+    #[serde(skip_serializing)]
+    id: AlertId,
+    routing_key: String,
+    event_action: EventAction,
+    #[serde(rename = "payload.summary")]
+    payload_summary: String,
+    #[serde(rename = "payload.source")]
+    payload_source: String,
+    #[serde(rename = "payload.severity")]
+    payload_severity: PayloadSeverity,
 }
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum EventAction {
-	Trigger,
-	Acknowledge,
-	Resolve,
+    Trigger,
+    Acknowledge,
+    Resolve,
 }
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum PayloadSeverity {
-	Critical,
-	Error,
-	Warning,
-	Info,
+    Critical,
+    Error,
+    Warning,
+    Info,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ServiceConfig {
-	integration_key: String,
-	event_action: EventAction,
-	payload_source: String,
-	payload_severity: PayloadSeverity,
+    integration_key: String,
+    event_action: EventAction,
+    payload_source: String,
+    payload_severity: PayloadSeverity,
 }
 
 pub struct PagerDutyClient {
-	config: Vec<ServiceConfig>,
+    config: Vec<ServiceConfig>,
 }
 
 impl PagerDutyClient {
-	fn new_alerts(&self, alert: AlertContext) -> Vec<AlertEvent> {
-		let mut alerts = vec![];
+    fn new_alert_events(&self, notify: &NotifyAlert) -> Vec<AlertEvent> {
+        let mut alerts = vec![];
 
-		for config in &self.config {
-			alerts.push(
-				AlertEvent {
-					routing_key: config.integration_key.clone(),
-					event_action: config.event_action,
-					payload_summary: format!(""),
-					payload_source: config.payload_source.clone(),
-					payload_severity: config.payload_severity,
-				}
-			);
-		}
+        for config in &self.config {
+            for alert in &notify.alerts {
+                alerts.push(AlertEvent {
+                    id: alert.id.clone(),
+                    routing_key: config.integration_key.clone(),
+                    event_action: config.event_action,
+                    payload_summary: AlertContextTrimmed::from(alert.clone()).to_string(),
+                    payload_source: config.payload_source.clone(),
+                    payload_severity: config.payload_severity,
+                });
+            }
+        }
 
-		alerts
-	}
+        alerts
+    }
 }
 
 impl Default for PagerDutyClient {
@@ -83,14 +87,14 @@ impl Handler<NotifyAlert> for PagerDutyClient {
     type Result = ResponseActFuture<Self, Result<()>>;
 
     fn handle(&mut self, notify: NotifyAlert, _ctx: &mut Self::Context) -> Self::Result {
-		let f = async move {
+        let f = async move {
             if notify.alerts.is_empty() {
                 return Ok(());
             }
 
-			unimplemented!();
-		};
+            unimplemented!();
+        };
 
-		Box::pin(f.into_actor(self))
-	}
+        Box::pin(f.into_actor(self))
+    }
 }
