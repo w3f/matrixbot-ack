09df0ec78b8f9615c9f7fa7b9df969136e901356
lamafab <42901763+lamafab@users.noreply.github.com>
2022-06-08 16:18:40 +0200

    expand handling of UserConfirmation

--- src/user_request.rs
+++ src/user_request.rs
@@ -1,25 +1,53 @@
-use crate::primitives::{Command, UserAction, UserConfirmation};
+use crate::database::Database;
+use crate::escalation::EscalationService;
+use crate::primitives::{Acknowledgement, Command, UserAction, UserConfirmation};
 use crate::Result;
 use actix::prelude::*;
 
-pub struct RequestHandler {}
+pub struct RequestHandler<T: Actor> {
+    escalation_service: Addr<T>,
+    db: Database,
+}
 
-impl Actor for RequestHandler {
+impl<T: Actor> Actor for RequestHandler<T> {
     type Context = Context<Self>;
 }
 
-impl Handler<UserAction> for RequestHandler {
+impl<T> Handler<UserAction> for RequestHandler<T>
+where
+    T: Actor + Handler<Acknowledgement>,
+    <T as Actor>::Context: actix::dev::ToEnvelope<T, Acknowledgement>,
+{
     type Result = ResponseActFuture<Self, Result<UserConfirmation>>;
 
     fn handle(&mut self, msg: UserAction, ctx: &mut Self::Context) -> Self::Result {
-        let f = async move {
-            match msg.command {
-                Command::Ack(alert_id) => {}
-                Command::Pending => {}
-                Command::Help => {}
-            }
+        let escalation_service = self.escalation_service.clone();
+        let db = self.db.clone();
 
-            unimplemented!()
+        let f = async move {
+            let res = match msg.command {
+                Command::Ack(alert_id) => {
+                    // TODO: Handle unwrap
+                    let x = escalation_service
+                        .send(Acknowledgement {
+                            user: msg.user,
+                            channel_id: msg.channel_id,
+                            alert_id,
+                        })
+                        .await;
+
+                    UserConfirmation::AlertAcknowledged(alert_id)
+                }
+                Command::Pending => {
+                    // TODO: Handle unwrap.
+                    let pending = db.get_pending(None).await.unwrap();
+
+                    UserConfirmation::PendingAlerts(pending.contexts_owned())
+                }
+                Command::Help => UserConfirmation::Help,
+            };
+
+            Ok(res)
         };
 
         Box::pin(f.into_actor(self))
